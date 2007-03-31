12c53e8b17e99e91061622479483a59e03f626a3
hmng <hmng@9fba729e-1ea9-4a68-a154-97c87594785d>
2007-03-31 20:55:54 +0000

    Registers device, reads return EOF.
    git-svn-id: file:///Volumes/farol/scm/bctadc@2 9fba729e-1ea9-4a68-a154-97c87594785d

--- bctadc.c
+++ bctadc.c
@@ -1,25 +1,69 @@
 #include <linux/init.h> 
 #include <linux/module.h> 
 #include <linux/pci.h>
+#include <linux/fs.h> 
 
 MODULE_LICENSE("GPL"); 
 
+static int bctadc_major = 0;
+
+//we need this 3 IOPORTS to read from the board:
+static unsigned long conversionControl,inputSelect,inputSample;
+
 static struct pci_device_id bctadc_ids [] = {
-	{ PCI_DEVICE(0x1102,0x7002) },
-	{ PCI_DEVICE(0x0005,0x0584) } ,
+	{ PCI_DEVICE(0x13c7,0x0adc) },
 	{ 0 ,}
 };
 
 MODULE_DEVICE_TABLE(pci,bctadc_ids);
 
+ssize_t bctadc_read (struct file *filp, char __user *buf, size_t count, loff_t *pos)
+{
+	printk(KERN_DEBUG "process %i (%s) going to read\n",
+			current->pid, current->comm);
+	return 0; /* EOF */
+}
+
+
+struct file_operations bctadc_fops = {
+	.owner = THIS_MODULE,
+	.read =  bctadc_read,
+};
+
+
+
 int probe (struct pci_dev *dev, const struct pci_device_id *id) {
-	printk( KERN_ALERT "Probed!!\n");
-return -1;
+	/* called by the kernel when the pci device is found */
+		
+	int result;
+	unsigned long bar;
+	printk( KERN_ALERT "BCTADC: Probed!!\n");
+	if(!pci_enable_device (dev)) { 
+		printk(KERN_ALERT "BCTADC: enabled\n");
+		//Now get tha IOPorts needed
+		bar=pci_resource_start(dev, 2);
+		conversionControl = bar +0xC;
+		inputSelect = bar + 0xD;
+		inputSample = pci_resource_start(dev,3); // +0x0
+		
+		/*
+		 * Register your major, and accept a dynamic number
+		 */
+		result = register_chrdev(bctadc_major, "bctadc", &bctadc_fops);
+		if (result < 0)
+			return result;
+		if (bctadc_major == 0)
+			bctadc_major = result; /* dynamic */
+		return 0;
+	} else
+		return -1;
 };
 
 
 void remove (struct pci_dev *dev) {
-
+	//what should we do here?? Is it ok to disable the device?
+	unregister_chrdev(bctadc_major, "bctadc");
+	pci_disable_device (dev);
 };
 
 static struct pci_driver pci_driver = {
@@ -31,13 +75,13 @@ static struct pci_driver pci_driver = {
  
 static int hello_init(void) 
 { 
-    printk(KERN_ALERT "Hello, world\n"); 
+    printk(KERN_ALERT "BTCADC Loaded\n"); 
     return pci_register_driver(&pci_driver); 
 } 
 
 static void hello_exit(void) 
 { 
-    printk(KERN_ALERT "Goodbye, cruel world\n"); 
+    printk(KERN_ALERT "BTCADC removed\n"); 
     pci_unregister_driver(&pci_driver);
 } 
 
